40c4dfe9fb4a1b9c45601fb9573e60aac140dde6
IrinaPresnyakova <91635867+IrinaPresnyakova@users.noreply.github.com>
2022-07-15 20:18:37 -0700

    Put together type kit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import './Header.scss'
+
+function Header() {
+    return (
+        <div className="header">
+          <h1>Hello world</h1>
+          <h1>Oh well</h1>
+        </div>
+    );
+}
+
+  export default Header 
